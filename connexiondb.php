81eb3464185f7eac0503cbc85d323ea96c1627ec
root <root@auriot>
2024-04-17 21:40:29 +0100

    insertion des produit

---
+++ connexiondb.php
@@ -0,0 +1,12 @@
+<?php 
+   function connexionMysqli(){
+    $conn= new mysqli('localhost','auriot','auriot','pharmacie');
+   	if ($conn){
+   		return $conn;
+   	}else{
+       echo "Could not connect to mysql".mysqli_error($con);
+       return false;
+   	} 
+   }
+   
+?>
