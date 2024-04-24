13269cb5b3cf58d294cfbbbd69c1cb765de8175f
BLackf535 <blackferdinand535@gmail.com>
2024-04-24 15:24:26 +0100

    approvisionnerment en fonction de fornisseur et liste facture

--- assets/vendor/alpha/facture.php
+++ assets/vendor/alpha/facture.php
@@ -1,176 +1,181 @@
 <?php
 require('alphapdf.php');
 
-// Créer un nouvel objet PDF
-class CustomPDF extends AlphaPDF
-{
-    // Fonction d'en-tête
-    function Header()
-    {
-        // Logo (à remplacer par le chemin de votre logo)
-        $this->Image('../../img/pharma.jpeg',0,0,0);
-
-        $this->SetAlpha(0.20);
-        $this->Image('../../img/pharma.jpeg', 0, 0, $this->GetPageWidth(), $this->GetPageHeight());
-        $this->SetAlpha(1);
-        // Adresse de l'entreprise
-        $this->Cell(80);
-        $this->SetFont('Arial','B',40);
-        $this->SetTextColor(0, 100, 0);
-        $this->Cell(90, 30, 'FACTURE', 0, 1, 'R'); 
-        $this->Ln();
-        $this->SetXY(120, 40);
-        $this->SetFont('Arial','',14);
-        $this->SetTextColor(0, 0, 0);
-        $this->SetFont('Arial','B',14);
-        $this->Cell(90, 0, utf8_decode('Numéro de facture:'), 0, 1, 'L');
-        $this->SetFont('Arial','I',14);
-        $this->Cell( 0, 0, 'id-90', 0, 1, 'R');
-        $this->Ln();
-        $this->SetXY(120, 47);
-        $this->SetFont('Arial','B',14);
-        $this->Cell(90, 0, 'Date de facturation:', 0, 1, 'L');
-        $this->SetFont('Arial','I',14);
-        $this->Cell(0, 0, '12-12-2024', 0, 1, 'R');
-        $this->SetXY(120, 54);
-        $this->SetFont('Arial','B',14);
-        $this->Cell(90, 0, utf8_decode('Échéance:'), 0, 1, 'L');
-        $this->SetFont('Arial','I',14);
-        $this->Cell(0, 0, '12-12-2024', 0, 1, 'R');
-        $this->Ln(20);
-    }
-
-    // Fonction de pied de page
-    function Footer()
-    {
-        // Position à 15 mm du bas
-        $this->SetY(-15);
-
-        // Texte du pied de page
-        $this->SetFont('Arial', 'I', 14);
-        $this->Cell(0, 10, 'Merci d\'etre passe chez nous', 0, 0, 'C');
-    }
-
-    // Chargement des données
-    function LoadData()
-    {
-        // Ajouter le corps du tableau
-        $data = array(
-            array('', 'Article 1', '9', '350,00', '3 150,00'),
-            array('', 'Article 1', '9', '350,00', '3 150,00'),
-            array('', 'Article 1', '9', '350,00', '3 150,00'),
-            array('', 'Article 1', '9', '350,00', '3 150,00'),
-            array('', 'Article 1', '9', '350,00', '3 150,00'),
-            array('', 'Article 2', '7', '120,00', '840,00'),
-            array('', 'Article 2', '7', '120,00', '840,00'),
-            array('', 'Article 2', '7', '120,00', '840,00'),
-            array('', 'Article 2', '7', '120,00', '840,00'),
-            array('', 'Article 2', '7', '120,00', '840,00'),
-            array('', 'Article 2', '7', '120,00', '840,00'),
-            array('', 'Article 2', '7', '120,00', '840,00')
-            // ...
-        );
-        return $data;
-    }
-
-    // Tableau personnalisé
-    function FancyTable($header, $data)
-    {
-        // Couleurs, épaisseur du trait et police en gras
-        $this->SetFillColor(0, 100, 0); 
-        $this->SetTextColor(255);
-        // $this->SetDrawColor(128, 0, 0);
-        $this->SetLineWidth(.3);
-        $this->SetFont('', 'B');
-
-        // En-tête du tableau
-        $w = array(25, 75, 25, 30, 35);
-        for ($i = 0; $i < count($header); $i++) {
-            $this->Cell($w[$i], 8, $header[$i], 1, 0, 'C', true);
-        }
-        $this->Ln();
-
-        // Restauration des couleurs et de la police
-        $this->SetFillColor(224, 235, 255);
-        $this->SetTextColor(0);
-        $this->SetFont('');
-
-        $n = 1;
-        // Données du tableau
-        foreach ($data as $row) {
-            $this->Cell($w[0],7,$n,'LRT',0,'C');
-            $this->Cell($w[1],7,utf8_decode($row[1]),'LRT',0,'L');
-            $this->Cell($w[2],7,utf8_decode($row[2]),'LRT',0,'R');
-            $this->Cell($w[3],7,utf8_decode($row[3]),'LRT',0,'R');
-            $this->Cell($w[4],7,utf8_decode($row[4]),'LRT',0,'R');
-            $this->Ln();
-            $n++;
-        }
-
-        // Trait de fermeture
-        $this->Cell(array_sum($w), 0, '', 'T');
-    }
+if(isset($_POST['IDfact'])) {
+    // Récupérer l'ID de l'utilisateur à supprimer depuis les données POST
+    $factid = $_POST['IDfact'];
+    echo $factid; 
 }
+// // Créer un nouvel objet PDF
+// class CustomPDF extends AlphaPDF
+// {
+//     // Fonction d'en-tête
+//     function Header()
+//     {
+//         // Logo (à remplacer par le chemin de votre logo)
+//         $this->Image('../../img/pharma.jpeg',0,0,0);
 
-$pdf = new CustomPDF(); // Création d'une instance de la classe CustomPDF
-$pdf->AliasNbPages(); // Gestion automatique du nombre de pages
+//         $this->SetAlpha(0.20);
+//         $this->Image('../../img/pharma.jpeg', 0, 0, $this->GetPageWidth(), $this->GetPageHeight());
+//         $this->SetAlpha(1);
+//         // Adresse de l'entreprise
+//         $this->Cell(80);
+//         $this->SetFont('Arial','B',40);
+//         $this->SetTextColor(0, 100, 0);
+//         $this->Cell(90, 30, 'FACTURE', 0, 1, 'R'); 
+//         $this->Ln();
+//         $this->SetXY(120, 40);
+//         $this->SetFont('Arial','',14);
+//         $this->SetTextColor(0, 0, 0);
+//         $this->SetFont('Arial','B',14);
+//         $this->Cell(90, 0, utf8_decode('Numéro de facture:'), 0, 1, 'L');
+//         $this->SetFont('Arial','I',14);
+//         $this->Cell( 0, 0, 'id-90', 0, 1, 'R');
+//         $this->Ln();
+//         $this->SetXY(120, 47);
+//         $this->SetFont('Arial','B',14);
+//         $this->Cell(90, 0, 'Date de facturation:', 0, 1, 'L');
+//         $this->SetFont('Arial','I',14);
+//         $this->Cell(0, 0, '12-12-2024', 0, 1, 'R');
+//         $this->SetXY(120, 54);
+//         $this->SetFont('Arial','B',14);
+//         $this->Cell(90, 0, utf8_decode('Échéance:'), 0, 1, 'L');
+//         $this->SetFont('Arial','I',14);
+//         $this->Cell(0, 0, '12-12-2024', 0, 1, 'R');
+//         $this->Ln(20);
+//     }
 
-$pdf->AddPage(); // Ajout d'une page
+//     // Fonction de pied de page
+//     function Footer()
+//     {
+//         // Position à 15 mm du bas
+//         $this->SetY(-15);
 
-// Données du client
-$pdf->SetFont('Arial','B',20); 
-$pdf->SetTextColor(0, 100, 0); 
-$pdf->Cell(95, 10, utf8_decode('Facturé à:'), 0, 1, 'L'); 
-$pdf->SetTextColor(0, 0, 0); 
-$pdf->SetFont('Arial', 'B', 14); 
-$pdf->Cell(30, 7, 'Nom: ', 0, 0, 'L'); 
-$pdf->SetFont('Arial', '', 14); 
-$pdf->Cell(80, 7, utf8_decode('Jhon Doe'), 0, 1, 'L'); 
-$pdf->SetFont('Arial', 'B', 14); 
-$pdf->Cell(30, 7, 'Ville: ', 0, 0, 'L'); 
-$pdf->SetFont('Arial', '', 14); 
-$pdf->Cell(95, 7, utf8_decode('Bafoussam'), 0, 1, 'L'); 
-$pdf->SetFont('Arial', 'B', 14); 
-$pdf->Cell(30, 7, 'Email: ', 0, 0, 'L'); 
-$pdf->SetFont('Arial', '', 14); 
-$pdf->Cell(95, 7, utf8_decode('adresse@gmial.com'), 0, 1, 'L'); 
-$pdf->SetFont('Arial', 'B', 14); 
-$pdf->Cell(30, 7, 'Telephone: ', 0, 0, 'L'); 
-$pdf->SetFont('Arial', '', 14); 
-$pdf->Cell(95, 7, utf8_decode('698989898'), 0, 1, 'L'); 
-$pdf->Ln(10); 
+//         // Texte du pied de page
+//         $this->SetFont('Arial', 'I', 14);
+//         $this->Cell(0, 10, 'Merci d\'etre passe chez nous', 0, 0, 'C');
+//     }
 
-// En-tête du tableau
-$header = array('n', utf8_decode('Désignation'), utf8_decode('Quantité'),  'Prix unitaire', 'Montant');
-$pdf->SetFont('Arial', 'B', 14);
-$pdf->FancyTable($header, $pdf->LoadData());
-$pdf->Ln(); 
-$pdf->Cell(115);  // Cellule vide pour déplacer vers la droite
-$pdf->Cell(40, 7, 'Montant HT: ', 'LRT', 0, 'L'); 
-$pdf->Cell(35, 7, '234456', 'LRT', 1, 'R'); 
+//     // Chargement des données
+//     function LoadData()
+//     {
+//         // Ajouter le corps du tableau
+//         $data = array(
+//             array('', 'Article 1', '9', '350,00', '3 150,00'),
+//             array('', 'Article 1', '9', '350,00', '3 150,00'),
+//             array('', 'Article 1', '9', '350,00', '3 150,00'),
+//             array('', 'Article 1', '9', '350,00', '3 150,00'),
+//             array('', 'Article 1', '9', '350,00', '3 150,00'),
+//             array('', 'Article 2', '7', '120,00', '840,00'),
+//             array('', 'Article 2', '7', '120,00', '840,00'),
+//             array('', 'Article 2', '7', '120,00', '840,00'),
+//             array('', 'Article 2', '7', '120,00', '840,00'),
+//             array('', 'Article 2', '7', '120,00', '840,00'),
+//             array('', 'Article 2', '7', '120,00', '840,00'),
+//             array('', 'Article 2', '7', '120,00', '840,00')
+//             // ...
+//         );
+//         return $data;
+//     }
 
-$pdf->Cell(115);  // Cellule vide pour déplacer vers la droite
-$pdf->Cell(40, 7, 'TVA (19,25%): ', 'LRT', 0, 'L'); 
-$pdf->Cell(35, 7, '345.2', 'LRT', 1, 'R');
+//     // Tableau personnalisé
+//     function FancyTable($header, $data)
+//     {
+//         // Couleurs, épaisseur du trait et police en gras
+//         $this->SetFillColor(0, 100, 0); 
+//         $this->SetTextColor(255);
+//         // $this->SetDrawColor(128, 0, 0);
+//         $this->SetLineWidth(.3);
+//         $this->SetFont('', 'B');
 
-$pdf->SetTextColor(255);
-$pdf->SetLineWidth(.3);
-$pdf->SetFont('', 'B');
-$pdf->SetFillColor(0, 100, 0); 
-$pdf->Cell(115);  // Cellule vide pour déplacer vers la droite
-$pdf->Cell(40, 8, 'TOTAL TTC: ', 'LRTB', 0, 'L', true); 
-$pdf->Cell(35, 8, '987645', 'LRTB', 1, 'R', true); 
-// Restauration des couleurs et de la police
-$pdf->SetFillColor(224, 235, 255);
-$pdf->SetTextColor(0);
-$pdf->SetFont('');
+//         // En-tête du tableau
+//         $w = array(25, 75, 25, 30, 35);
+//         for ($i = 0; $i < count($header); $i++) {
+//             $this->Cell($w[$i], 8, $header[$i], 1, 0, 'C', true);
+//         }
+//         $this->Ln();
 
-$pdf->Ln(10);
-$pdf->Cell(20);
-$pdf->SetFont('', 'B');
-$pdf->Cell(40, 8, 'Signature Client', 'B', 0, 'L'); 
-$pdf->Cell(50);
-$pdf->Cell(40, 8, 'Signature Caisse', 'B', 1, 'L'); 
+//         // Restauration des couleurs et de la police
+//         $this->SetFillColor(224, 235, 255);
+//         $this->SetTextColor(0);
+//         $this->SetFont('');
 
-$pdf->Output(); // Génération du document PDF
+//         $n = 1;
+//         // Données du tableau
+//         foreach ($data as $row) {
+//             $this->Cell($w[0],7,$n,'LRT',0,'C');
+//             $this->Cell($w[1],7,utf8_decode($row[1]),'LRT',0,'L');
+//             $this->Cell($w[2],7,utf8_decode($row[2]),'LRT',0,'R');
+//             $this->Cell($w[3],7,utf8_decode($row[3]),'LRT',0,'R');
+//             $this->Cell($w[4],7,utf8_decode($row[4]),'LRT',0,'R');
+//             $this->Ln();
+//             $n++;
+//         }
+
+//         // Trait de fermeture
+//         $this->Cell(array_sum($w), 0, '', 'T');
+//     }
+// }
+
+// $pdf = new CustomPDF(); // Création d'une instance de la classe CustomPDF
+// $pdf->AliasNbPages(); // Gestion automatique du nombre de pages
+
+// $pdf->AddPage(); // Ajout d'une page
+
+// // Données du client
+// $pdf->SetFont('Arial','B',20); 
+// $pdf->SetTextColor(0, 100, 0); 
+// $pdf->Cell(95, 10, utf8_decode('Facturé à:'), 0, 1, 'L'); 
+// $pdf->SetTextColor(0, 0, 0); 
+// $pdf->SetFont('Arial', 'B', 14); 
+// $pdf->Cell(30, 7, 'Nom: ', 0, 0, 'L'); 
+// $pdf->SetFont('Arial', '', 14); 
+// $pdf->Cell(80, 7, utf8_decode('Jhon Doe'), 0, 1, 'L'); 
+// $pdf->SetFont('Arial', 'B', 14); 
+// $pdf->Cell(30, 7, 'Ville: ', 0, 0, 'L'); 
+// $pdf->SetFont('Arial', '', 14); 
+// $pdf->Cell(95, 7, utf8_decode('Bafoussam'), 0, 1, 'L'); 
+// $pdf->SetFont('Arial', 'B', 14); 
+// $pdf->Cell(30, 7, 'Email: ', 0, 0, 'L'); 
+// $pdf->SetFont('Arial', '', 14); 
+// $pdf->Cell(95, 7, utf8_decode('adresse@gmial.com'), 0, 1, 'L'); 
+// $pdf->SetFont('Arial', 'B', 14); 
+// $pdf->Cell(30, 7, 'Telephone: ', 0, 0, 'L'); 
+// $pdf->SetFont('Arial', '', 14); 
+// $pdf->Cell(95, 7, utf8_decode('698989898'), 0, 1, 'L'); 
+// $pdf->Ln(10); 
+
+// // En-tête du tableau
+// $header = array('n', utf8_decode('Désignation'), utf8_decode('Quantité'),  'Prix unitaire', 'Montant');
+// $pdf->SetFont('Arial', 'B', 14);
+// $pdf->FancyTable($header, $pdf->LoadData());
+// $pdf->Ln(); 
+// $pdf->Cell(115);  // Cellule vide pour déplacer vers la droite
+// $pdf->Cell(40, 7, 'Montant HT: ', 'LRT', 0, 'L'); 
+// $pdf->Cell(35, 7, '234456', 'LRT', 1, 'R'); 
+
+// $pdf->Cell(115);  // Cellule vide pour déplacer vers la droite
+// $pdf->Cell(40, 7, 'TVA (19,25%): ', 'LRT', 0, 'L'); 
+// $pdf->Cell(35, 7, '345.2', 'LRT', 1, 'R');
+
+// $pdf->SetTextColor(255);
+// $pdf->SetLineWidth(.3);
+// $pdf->SetFont('', 'B');
+// $pdf->SetFillColor(0, 100, 0); 
+// $pdf->Cell(115);  // Cellule vide pour déplacer vers la droite
+// $pdf->Cell(40, 8, 'TOTAL TTC: ', 'LRTB', 0, 'L', true); 
+// $pdf->Cell(35, 8, '987645', 'LRTB', 1, 'R', true); 
+// // Restauration des couleurs et de la police
+// $pdf->SetFillColor(224, 235, 255);
+// $pdf->SetTextColor(0);
+// $pdf->SetFont('');
+
+// $pdf->Ln(10);
+// $pdf->Cell(20);
+// $pdf->SetFont('', 'B');
+// $pdf->Cell(40, 8, 'Signature Client', 'B', 0, 'L'); 
+// $pdf->Cell(50);
+// $pdf->Cell(40, 8, 'Signature Caisse', 'B', 1, 'L'); 
+
+// $pdf->Output(); // Génération du document PDF
 ?>
---
+++ listeFacture.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "tet.php"?>
+
+<?php include "include/header.php"?>
+  <!-- datatable link -->
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css"> -->
+  <!-- <link rel="stylesheet" href="https://cdn.datatables.net/2.0.4/css/dataTables.bootstrap5.css"> -->
+  <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script> -->
+  <script src="https://cdn.datatables.net/2.0.4/js/dataTables.js"></script>
+  <script src="https://cdn.datatables.net/2.0.4/js/dataTables.bootstrap5.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+<body>
+<!-- navbar -->
+<?php include "include/navbar.php"?>
+
+<!-- sidebar -->
+<?php include "include/sidebar.php"?>
+
+<!-- contain page -->
+<main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Dashboard</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item active">Dashboard</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section dashboard">
+        <div class="row">
+            <!-- <div class="col-lg-2"></div> -->
+            <div class="col-lg-12  card">
+                <div class="row">
+                    <table id="example" class="table table-striped" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>IDVente</th>
+                                <th>dateVente</th>
+                                <th>qteVendu</th>
+                                <th>numFactur</th>
+                                <th>Nom Produit</th>
+                                <th>Client</th>
+                                <th>Action</th>                                                                                                                                                                                                                                                                                                                                      
+                            </tr>
+                        </thead>
+                        <tbody>
+                         
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+</main><!-- End #main -->
+
+<!-- start modal to delete -->
+<!-- <div class="modal fade" id="delete" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Delect Admin</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+        <div class="modal-body">
+            <form class="row g-3 needs-validation" id="myform" novalidate>
+                <div class="col-md-12">
+                    <label for="nomProd" class="form-label">Nom Admin:</label>
+                    <input type="text" class="form-control" id="nomProd" name="nomProd" placeholder="enter un nom" required>
+                    <div class="invalid-feedback">
+                    nom produit requis!
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <label for="numLot" class="form-label">Login :</label>
+                    <input type="text" class="form-control" id="numLot" name="numLot" placeholder="" required>
+                    <div class="invalid-feedback">
+                    numero de lot requis
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer d-flex justify-content-center">
+                <div class="col-12">
+                    <div class="d-flex justify-content-center">
+                    <button class="btn btn-outline-danger" data-bs-dismiss="modal">cancel</button>
+                    <button class="btn btn-outline-primary mx-3" type="submit" id="btn-send">Supprimer</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+  </div>
+</div> -->
+<!--end modal to delete -->
+ 
+<?php include "include/footer.php";?>   
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="script/script.js"></script>
+<script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/chart.js/chart.umd.js"></script>
+<script src="assets/vendor/echarts/echarts.min.js"></script>
+<script src="assets/vendor/quill/quill.min.js"></script>
+<script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+<script src="assets/vendor/tinymce/tinymce.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
+<script>
+    $(document).ready(function () {
+        // Chargement des données de la base de données lors du chargement de la page
+        $.ajax({
+            url: 'getVenteData.php', // Le script PHP pour récupérer les données de la base de données
+            type: 'GET',
+            success: function (response) {
+                // Ajout des éléments récupérés dans le tableau
+                $('tbody').html(response);
+            },
+            error: function (xhr, status, error) {
+                console.error('Une erreur est survenue lors du chargement des données.');
+            }
+        });
+
+        var IdFactur ; // Variable pour stocker l'ID de l
+
+        $(document).on('click', '.btn-facture', function () {
+            IdFactur = $(this).data('fact-id'); // Récupérer l'ID
+            var rowData = $(this).closest('tr').find('td'); // Récupérer les données de la ligne sélectionnée
+
+            // Afficher les données de l'utilisateur dans le modal
+           
+
+           // $('#delete').modal('show'); // Afficher le modal de confirmation
+
+
+                         $.ajax({
+                                    url: 'assets/vendor/alpha/facture.php', // facture
+                                    type: 'POST',
+                                    data: { IDfact: IdFactur }, // Passer l'ID de la facture
+                                    
+                                    error: function (xhr, status, error) {
+                                        console.error('Une erreur .');
+                                        Swal.fire(
+                                            'Erreur!',
+                                            'Une erreur .',
+                                            'error'
+                                        );
+                                    }
+                                });
+        });
+           
+                    // Effectuer une requête AJAX pour 
+                                
+          
+    });
+</script>
+</body>
+</html>
